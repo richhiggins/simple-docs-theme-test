8a0e09dc22dd04c24be2de391183855c19d73f75
Rich Higgins <hello@iamrichhiggins.com>
2019-09-23 22:19:55 +0100

    updated dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
     description:
       "A collaboratively developed, universal content style guide, based on usability evidence."
   },
-  __experimentalThemes: ["gatsby-theme-simple-docs"],
-  plugins: [`gatsby-plugin-catch-links`, `gatsby-plugin-netlify-cms`]
+  plugins: [
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-theme-simple-docs`
+  ]
 };
