d46c7e4752c34671e03f0110427c05d5cb327892
Rich Higgins <hello@iamrichhiggins.com>
2019-10-09 21:08:01 +0100

    successfully added a shadow template 🎉

---
+++ src/gatsby-theme-simple-docs/templates/doc.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import MDXRenderer from "gatsby-mdx/mdx-renderer";
+import Layout from "gatsby-theme-simple-docs/src/components/layout";
+
+export const query = graphql`
+  query($slug: String!) {
+    mdx(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        description
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+
+export default ({ data }) => (
+  <Layout>
+    <Helmet>
+      <title>{data.mdx.frontmatter.title}</title>
+      <meta name="description" content={data.mdx.frontmatter.description} />
+    </Helmet>
+    <p style={{ color: "orange" }}>[shadowed theme template]</p>
+    <h1>{data.mdx.frontmatter.title}</h1>
+    <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+  </Layout>
+);
